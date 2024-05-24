cd8184b5b4e5597bc9e1b32f2e8b0d3451d34e26
nyxxxxx <s0941212@gmail.com>
2024-05-24 15:53:16 +0800

    update hachi.gif & button style

--- VueTutorial/src/App.vue
+++ VueTutorial/src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <!-- *Remember App.vue is the root component of all. -->
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <!-- Import img from public/ -->
-      <img src="/test/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <!-- Import img from ./src/assets/img -->
-      <img src="@img/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="@img/hachi.gif" class="logo vue" alt="Vue logo" />
   </div>
   <!-- Children component -->
   <Directives></Directives>
@@ -25,7 +18,7 @@ import Directives from './components/DirectivesExample.vue'
   padding: 1.5em;
   will-change: filter;
 }
-.logo:hover {
+.logo.vite:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
